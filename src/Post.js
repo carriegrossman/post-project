cca71d3fa7ad54618eee495810f3d61db3e25bce
Carrie Grossman <carriegrossman@MacBook-Pro.local>
2020-08-18 15:05:26 -0400

    updated

--- src/Post.js
+++ src/Post.js
@@ -1,22 +1,29 @@
-import React from "react";
+import React, {useState, useEffect} from "react"
 
-// const Post =(props) =>{
-//     return(
-//         <li>
-//             <div>{props.post.title}</div>
-//         </li>
-//     )
-// }
+const Post = ({post, setActive, isActive})=>{
 
-//deconsturing to just handle posts
-const Post = ({post})=>{
+    const [comments, setComments] = useState([]);
+
+    useEffect(()=>{
+        const {id} = post;
+        if(!comments.length > 0 && isActive){
+            fetch(`https://jsonplaceholder.typicode.com/posts/${id}/comments`)
+            .then(resp=>resp.json())
+                .then(data=>setComments(data))
+            }
+    },[comments, isActive, post])
+    
     return(
-        <li id={'post_'+post.id}>
+        <li onClick={()=>setActive(post.id)} id={'post_'+post.id}>
             <h3>{post.title}</h3>
             <div>{post.body}</div>
             <aside>{post.userId}</aside>
+            <div>{isActive &&
+            comments.map(comment=><div key={comment.id}>{comment.id}</div>)}
+            </div>
         </li>
     )
+        
 }
 
 export default Post;
--- src/app2.js
+++ src/app2.js
@@ -9,42 +9,45 @@ const App = () => {
   const [filteredPosts, setFilteredPosts] = useState([]);
   const [posts, setPosts] = useState([]);
   const [activePost, setActivePost] = useState(null);
-  
+  const [isLoading, setStatus] = useState(null);
 
   useEffect(()=>{
+    if (isLoading) {
+      console.log("is loading")
       fetch("https://jsonplaceholder.typicode.com/posts/")
       .then(resp=>resp.json())
       .then(data=>{
-        setFilteredPosts(data)
+        setStatus(false)
         setPosts(data)
+        setFilteredPosts(data)
      })
+    }
+  }, [isLoading]) //only runs when isLoading is changed
 
-  }, []) //only runs when isLoading is changed
 
-
-  const whenUserIdSelected = (user) => {
+  const whenUserIdSelected=(user)=>{
     let selectedUser = user;
-    setFilteredPosts(posts.filter(post=>post.userId === selectedUser))
+    let filteredPosts = posts.filter(post=>post.userId === selectedUser)
+    setFilteredPosts(filteredPosts)
   }
-
 
   const setActive=(postId)=>{
     setActivePost(postId)
   }
 
-
-  let userIds=[...new Set(posts.map(post=>post.userId))]
-
-
     return (
       <div className="main-content">
         <div>
-          <SelectUserDropdown userIds={userIds} whenSelected={whenUserIdSelected} />
+          {
+          /*userIds.map(uid=><UserButton whenClicked={this.whenUserIdSelected.bind(this)} key={uid} user={uid}/>)*/
+          }
+          <SelectUserDropdown userIds={[...new Set(posts.map(post=>post.userId))]} whenSelected={whenUserIdSelected} />
           <button onClick={()=>setFilteredPosts(posts)} >reset</button>
         </div> 
 
         <ul>
-          {filteredPosts.map(post=>(
+          {isLoading ? "loading..." :
+          filteredPosts.map(post=>(
             <Post 
               setActive={setActive} 
               key={post.id} 
